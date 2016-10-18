845d885ffa5728f9f4914de3378aa201a05120ad
wangjhstc <wangjhstc@126.com>
2016-10-18 14:57:16 +0800

    #add：新增商品模型

--- SparrowModel/Src/Commodity/MCommodity.h
+++ SparrowModel/Src/Commodity/MCommodity.h
@@ -8,15 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum : NSUInteger {
+    /*! 普通商品 */
+    CommodityTypeOfNormal,
+    /*! 套餐商品 */
+    CommodityTypeOfCombo,
+    /*! 规格商品 */
+    CommodityTypeOfSpec,
+} CommodityType;
+
 @interface MCommodity : NSObject
 
+/*! 商品编号 */
+@property   (nonatomic,copy)    NSString    *ID;
+/*! 商品计价类型 */
+@property   (nonatomic,copy)    NSString    *type;
 /*! 商品名称 */
 @property   (nonatomic,copy)    NSString    *name;
-/*! 保质期 */
-@property   (nonatomic,copy)    NSString    *shelfLife;
-/*! 卫生标准号 ex:GB15979 */
-@property   (nonatomic,copy)    NSString    *QSCode;
-/*! 商品代码 */
-@property   (nonatomic,assign)  long        code;
+/*! 商品单位 */
+@property   (nonatomic,copy)    NSString    *unit;
+/*! 商品价格 */
+@property   (nonatomic,copy)    NSDecimalNumber    *price;
+/*! 商品数量 */
+@property   (nonatomic,copy)    NSDecimalNumber    *quantity;
+/*! 商品唯一标识 */
+@property   (nonatomic,copy)    NSString    *identifer;
+/*! 是否选中 */
+@property   (nonatomic,assign)  BOOL        isSelected;
+/*! 是否套餐 */
+@property   (nonatomic,assign)  BOOL        isCombo;
+
+@property   (nonatomic,assign)  CommodityType   commodityType;
 
 @end
---
+++ SparrowModelTests/SparrowModelTests.m
@@ -0,0 +1,39 @@
+//
+//  SparrowModelTests.m
+//  SparrowModelTests
+//
+//  Created by microsparrow on 16/9/30.
+//  Copyright © 2016年 microsparrow. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SparrowModelTests : XCTestCase
+
+@end
+
+@implementation SparrowModelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
